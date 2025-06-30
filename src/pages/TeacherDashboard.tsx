726854592c9012c81c32d6228c5eee8a83cc06f9
Chen Enjiao (Ernie) <erniesg@users.noreply.github.com>
2025-06-30 23:18:23 +0800

    fixed building error

--- src/pages/TeacherDashboard.tsx
+++ src/pages/TeacherDashboard.tsx
@@ -386,8 +386,7 @@ const TeacherDashboard: React.FC = () => {
               </button>
             </div>
           </div>
-            )}
-          </div>
+        </div>
 
           {/* Recent Materials */}
           <div className="bg-white rounded-xl shadow-sm border border-gray-100 p-6">
